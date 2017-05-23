f766b6e5e326dcb3d95903c1742ab80f92fbf179
Xiao Ruiguo <705437650@qq.com>
2017-05-23 11:34:33 +0800

    modify yum only centos

--- collect-config-setup/fragments/install_config_agent_yum.sh
+++ collect-config-setup/fragments/install_config_agent_yum.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 set -eux
+sed -i '$a proxy=http://opscloud:cloud0518@10.53.13.245:3128/' /etc/yum.conf
 
-# OSP_VERSION is set by ENV or by heat string replacement
-[ -n "$OSP_VERSION" ] || (echo "Missing required value OSP_VERSION" ; exit 1)
-
-# on Atomic host os-collect-config runs inside a container which is
-# fetched&started in another step
-[ -e /run/ostree-booted ] && exit 0
-
-if ! yum info os-collect-config; then
-    # if os-collect-config package is not available, first check if
-    # the repo is available but disabled, otherwise install the package
-    # from epel
-    if yum repolist disabled|grep rhel-7-server-openstack-$OSP_VERSION-rpms; then
-        subscription-manager repos --enable="rhel-7-server-openstack-$OSP_VERSION-rpms"
-        if [ "$OSP_VERSION" -lt 10 ] ; then
-            subscription-manager repos --enable="rhel-7-server-openstack-$OSP_VERSION-director-rpms"
-        fi
-    else
-        yum -y install centos-release-openstack-ocata
-    fi
-fi
+yum -y install https://www.rdoproject.org/repos/rdo-release.rpm
 yum -y install os-collect-config python-zaqarclient os-refresh-config os-apply-config openstack-heat-templates python-oslo-log python-psutil
 #yum-config-manager --disable 'epel*'
