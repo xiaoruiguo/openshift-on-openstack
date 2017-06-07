1920f1d3423795ce537a4532f09414d586fdaa06
Xiao Ruiguo <705437650@qq.com>
2017-06-07 22:50:26 +0800

    modify master.yaml 2.0 modify proxy.

--- ocata/fragments/set_proxy.sh
+++ ocata/fragments/set_proxy.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 sed -i '$a proxy=http://opscloud:cloud0518@10.53.13.245:3128/' /etc/yum.conf
 yum install wget git -y
 sed -i '$a https_proxy=http://opscloud:cloud0518@10.53.13.245:3128/' /etc/wgetrc
 sed -i '$a http_proxy =http://opscloud:cloud0518@10.53.13.245:3128/' /etc/wgetrc
 sed -i '$a ftp_proxy =http://opscloud:cloud0518@10.53.13.245:3128/' /etc/wgetrc
-# git config --global http.proxy  http://opscloud:cloud0518@10.53.13.245:3128
-# git config --global https.proxy  https://opscloud:cloud0518@10.53.13.245:3128
+git config --global http.proxy  http://opscloud:cloud0518@10.53.13.245:3128
+git config --global https.proxy  https://opscloud:cloud0518@10.53.13.245:3128
