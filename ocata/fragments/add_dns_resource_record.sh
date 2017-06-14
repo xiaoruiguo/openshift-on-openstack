b622210386e9dd4597f58a301b1de2d92ee0e2e4
Xiao Ruiguo <705437650@qq.com>
2017-06-14 22:32:55 +0800

    modify dns server.

--- ocata/fragments/add_dns_resource_record.sh
+++ ocata/fragments/add_dns_resource_record.sh
@@ -9,9 +9,9 @@ set -o pipefail
 
 # ansible all -i "localhost," -c local -m nsupdate -a "key_name='openshift.labs.local' key_secret=${key_secret} server='127.0.0.1' zone='openshift.labs.local' record='openshift-dns' value='10.238.121.4'"
 ansible all -i "localhost," -c local -m nsupdate -a "        \
-                           key_name=$domain_name           \
-                           key_secret=$dns_update_key      \
-                           server=$dns_nameserver          \
-                           zone=$domain_name               \
-                           record=$hostname                \
-                           value=$HOST_FIX_IP"
+                           key_name=$key_name           \
+                           key_secret=$key_secret      \
+                           server=$dns_server          \
+                           zone=$dns_zone               \
+                           record=$dns_record                \
+                           value=$dns_record_ip"
