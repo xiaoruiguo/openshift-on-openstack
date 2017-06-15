66d7fcc3974797d39be6a086e8b34d75e343191d
Xiao Ruiguo <705437650@qq.com>
2017-06-15 09:10:23 +0800

    modify dns

--- ocata/fragments/add_dns_resource_record.sh
+++ ocata/fragments/add_dns_resource_record.sh
@@ -7,10 +7,17 @@ set -eu
 set -x
 set -o pipefail
 
+if [ -e /var/named/K${key_name}*.private ]; then
+KEY="$(grep Key: /var/named/K${key_name}*.private | cut -d ' ' -f 2)"
+else
+echo "DNS Resource Record add Error."
+fi
+
+
 # ansible all -i "localhost," -c local -m nsupdate -a "key_name='openshift.labs.local' key_secret=${key_secret} server='127.0.0.1' zone='openshift.labs.local' record='openshift-dns' value='10.238.121.4'"
 ansible all -i "localhost," -c local -m nsupdate -a "        \
                            key_name=$key_name           \
-                           key_secret=$key_secret      \
+                           key_secret=$KEY      \
                            server=$dns_server          \
                            zone=$dns_zone               \
                            record=$dns_record                \
