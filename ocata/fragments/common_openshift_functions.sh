9beff3ca795f57978fabcbd4c0452443053fc1dd
Xiao Ruiguo <705437650@qq.com>
2017-06-28 21:07:52 +0800

    modify master.yaml 2.0 modify proxy

--- ocata/fragments/common_openshift_functions.sh
+++ ocata/fragments/common_openshift_functions.sh
@@ -10,15 +10,15 @@ function sudo_set_secure_path() {
 #
 
 function docker_install_and_enable() {
-
-cat <<EOF > /etc/yum.repos.d/docker.repo
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
+#
+#cat <<EOF > /etc/yum.repos.d/docker.repo
+#[dockerrepo]
+#name=Docker Repository
+#baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+#enabled=1
+#gpgcheck=1
+#gpgkey=https://yum.dockerproject.org/gpg
+#EOF
 
     if ! rpm -q docker
     then
